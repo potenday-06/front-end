dbdbe8f923af7361a1eabddb27dedbfbb0842870
hoon <dudrn587@gmail.com>
2025-03-02 00:40:11 +0900

    design: 온보딩 하단 footer lvh 적용

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -11,7 +11,7 @@ const Footer = ({ children, type }: FooterProps) => {
   const footerStyle = clsx(
     'fixed max-w-414 mx-auto bottom-0 left-0 right-0 flex flex-col h-[20svh] rounded-t-10 bg-white text-center text-black-10', // 기본 스타일
     {
-      'justify-between gap-31 h-[30svh] px-24 py-38': type === 'onboarding',
+      'justify-between gap-31 h-[30lvh] px-24 py-38': type === 'onboarding',
       'justify-between gap-31 h-[30svh] px-24 py-36': type === 'chatOnboarding',
       ' px-24 py-20 h-[30lvh]': type === 'chat',
     }
