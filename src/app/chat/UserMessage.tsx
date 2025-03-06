28172082c62d74804d7c0ea5eee279e6c0bd9a0f
hoon <dudrn587@gmail.com>
2025-03-06 21:31:36 +0900

    design: 유저 메시지 스타일 변경

--- src/app/chat/UserMessage.tsx
+++ src/app/chat/UserMessage.tsx
@@ -2,7 +2,7 @@ import { MessageProps } from './MessageWrapper'
 
 const UserMessage = ({ content }: Pick<MessageProps, 'content'>) => {
   return (
-    <div className='inline-block w-max max-w-[80%] rounded-30 border border-white bg-purple-5 px-16 py-8 text-left text-18-600'>
+    <div className='inline-block w-max max-w-[80%] rounded-10 border border-solid border-[#b49df1] bg-white px-16 py-8 text-left text-14-500 text-purple-40'>
       {content}
     </div>
   )
