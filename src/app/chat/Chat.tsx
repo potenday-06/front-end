9890e748917409b84b1d0039f2cab23de34f626f
hoon <dudrn587@gmail.com>
2025-03-08 12:12:05 +0900

    feat: 채팅, 요약 api 리팩토링 및 요약 화면 애니메이션 추가

--- src/app/chat/Chat.tsx
+++ src/app/chat/Chat.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image'
 
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 import MessageList from './MessageList'
 import ChatFooter from './ChatFooter'
@@ -13,6 +13,10 @@ import { postConversation } from '@/utils/api/conversation/postConversation'
 import { MessageType } from '@/utils/api/wholeConversation/getWholeConversation'
 
 import AIMessage from './AiMessage'
+import Lottie from 'lottie-react'
+import chatEndAnimation from '../../../public/assets/animation/chat-end.json'
+import { postMessage } from '@/utils/api/chat/postMessage'
+import { postSummary } from '@/utils/api/chat/postSummary'
 
 export type ChatMode = 'input' | 'choice' | 'end'
 
@@ -24,6 +28,7 @@ const Chat = () => {
 
   const [chatMode, setChatMode] = useState<ChatMode>('input')
   const [summary, setSummary] = useState('')
+  const [showAnimation, setShowAnimation] = useState(false)
 
   const onSubmit = async (
     inputMessage: string,
@@ -38,37 +43,18 @@ const Chat = () => {
     }
 
     setIsLoading(true)
+    const message = await postMessage(inputMessage)
 
-    try {
-      const response = await fetch('/api/completions', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ input: inputMessage }),
-      })
-
-      if (!response.ok) {
-        const errorText = await response.text()
-        console.error('API 호출 실패:', errorText)
-        return
-      }
-
-      const data = await response.json()
-
-      if (shouldAddUserMessage) {
-        const newAiMessage: MessageType = { type: 'AI', message: data.data }
-        setMessages((prev) => [...prev, newAiMessage])
-        setChatMode('choice')
-      } else {
-        setSummary(data.data)
-        return data.data
-      }
-    } catch (error) {
-      console.error('Fetch 오류:', error)
-    } finally {
-      setIsLoading(false)
+    if (shouldAddUserMessage) {
+      const newAiMessage: MessageType = { type: 'AI', message }
+      setMessages((prev) => [...prev, newAiMessage])
+      setChatMode('choice')
+    } else {
+      setSummary(message)
+      return message
     }
+
+    setIsLoading(false)
   }
 
   const handleMoreTalk = () => {
@@ -77,44 +63,36 @@ const Chat = () => {
 
   const handleStopTalk = async () => {
     setIsSubmitted(true)
+    const summaryData = await postSummary(messages)
 
-    try {
-      // 요약 API 호출
-      const response = await fetch('/api/summary', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ messages }),
-      })
+    // 요약된 데이터 DB 저장
+    await postConversation(messages, summaryData)
 
-      if (!response.ok) {
-        const errorText = await response.text()
-        console.error('요약 API 호출 실패:', errorText)
-        return
-      }
-
-      const { data: summaryData } = await response.json()
-
-      // 요약된 데이터 DB 저장
-      await postConversation(messages, summaryData)
-
-      setSummary(summaryData)
-      setChatMode('end') // 요약 화면으로 UI 변경
-    } catch (error) {
-      console.error('Fetch 오류:', error)
-    } finally {
-      setIsSubmitted(false)
-    }
+    setSummary(summaryData)
+    setChatMode('end') // 요약 화면으로 UI 변경
+    setIsSubmitted(false)
   }
 
-  const handleMoveHomePage = () => {
+  const handleMoveHomePage = useCallback(() => {
     router.push('/')
     router.refresh()
-  }
+  }, [router])
+
+  useEffect(() => {
+    let timer: ReturnType<typeof setTimeout>
+
+    if (showAnimation) {
+      timer = setTimeout(() => {
+        handleMoveHomePage()
+      }, 3000)
+    }
+
+    return () => clearTimeout(timer)
+  }, [showAnimation, router, handleMoveHomePage])
 
   useEffect(
     function scrollToBottom() {
+      if (showAnimation) return
       const header = document.getElementsByTagName('header')[0]
       const footerElement = document.getElementsByTagName('footer')[0]
         .firstChild as HTMLElement
@@ -129,6 +107,15 @@ const Chat = () => {
     [chatMode]
   )
 
+  if (showAnimation)
+    return (
+      <Lottie
+        animationData={chatEndAnimation}
+        loop={false}
+        className='absolute bottom-0 left-0 right-0 z-[9999]'
+      />
+    )
+
   return (
     <div className='bg-cloud-case1 flex h-svh flex-col'>
       <header className='flex items-center justify-center p-24'>
@@ -140,7 +127,14 @@ const Chat = () => {
             height={28}
           />
         )}
-        {chatMode === 'end' && <h1 className='text-24-700'>같이 별 남기기</h1>}
+        {chatMode === 'end' && (
+          <Image
+            src='/assets/icons/header-chat-end.svg'
+            alt='헤더'
+            width={99}
+            height={25}
+          />
+        )}
         <Image
           onClick={handleMoveHomePage}
           className='absolute right-[4%] cursor-pointer'
@@ -151,24 +145,24 @@ const Chat = () => {
         />
       </header>
 
+      {chatMode === 'end' && (
+        <main className='flex flex-1 flex-col items-center justify-between overflow-y-hidden px-24'>
+          <ChatSummary summary={summary} />
+          <Image
+            className='mb-[6svh] mt-24'
+            src='/assets/icons/tori-hug-star.svg'
+            alt='토리'
+            width={91}
+            height={112}
+          />
+        </main>
+      )}
       {chatMode !== 'end' && (
         <main
           className={`scrollbar-bar-hidden flex-1 overflow-y-auto overflow-x-hidden p-24 pt-48 [&::-webkit-scrollbar]:hidden`}
         >
           <AIMessage />
           <MessageList messages={messages} isLoading={isLoading} />
-        </main>
-      )}
-      {chatMode === 'end' && (
-        <main className='flex flex-1 flex-col items-center justify-between overflow-y-hidden px-24'>
-          <ChatSummary summary={summary} />
-          <Image
-            className='mt-24'
-            src='/assets/icons/tori-hug-star.svg'
-            alt='토리'
-            width={91}
-            height={112}
-          />
         </main>
       )}
 
@@ -190,7 +184,9 @@ const Chat = () => {
         )}
         {chatMode === 'end' && (
           <div className='p-24'>
-            <Button onClick={handleMoveHomePage}>오늘 대화 마치기</Button>
+            <Button onClick={() => setShowAnimation(true)}>
+              오늘 대화 마치기
+            </Button>
           </div>
         )}
       </footer>
