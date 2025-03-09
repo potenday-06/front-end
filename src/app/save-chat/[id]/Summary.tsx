5da895e3181dd326743cf5053391b8a801122472
hoon <dudrn587@gmail.com>
2025-03-09 16:18:10 +0900

    feat: 키워드 추출 기능 추가

--- src/app/save-chat/[id]/Summary.tsx
+++ src/app/save-chat/[id]/Summary.tsx
@@ -1,28 +1,54 @@
 'use client'
 
 import Button from '@/components/Button'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import WholeConversation from './WholeConversation'
 import { Conversation } from '@/utils/api/conversation/getConversation'
+import { postKeywords } from '@/utils/api/keywords/postKewords'
 
 const Summary = ({ conversation }: { conversation: Conversation }) => {
   const { summary, conversationId } = conversation
   const [displayWholeConversation, setDisplayWholeConversation] =
     useState(false)
+  const [keywords, setKeywords] = useState<string[]>([])
 
   const handleClick = () => {
     setDisplayWholeConversation(true)
   }
 
+  useEffect(() => {
+    const getKeywords = async () => {
+      const data = await postKeywords(summary)
+      const foramttedKeywords = data.result.text
+        .replace(/["\\]/g, '')
+        .split(',')
+        .map((keyword: string) => keyword.trim())
+
+      setKeywords(foramttedKeywords)
+    }
+
+    getKeywords()
+  }, [])
+
   return displayWholeConversation ? (
     <WholeConversation conversationId={conversationId} />
   ) : (
     <>
-      <main className='scrollbar-bar-hidden overflow-y-auto pt-24 text-16-500 [&::-webkit-scrollbar]:hidden'>
+      <main className='scrollbar-bar-hidden flex-1 overflow-y-auto pt-24 text-16-500 [&::-webkit-scrollbar]:hidden'>
         {summary}
       </main>
 
-      <footer className='fixed bottom-0 left-0 right-0 mx-auto max-w-414 p-16'>
+      <ul className='flex gap-12 py-24'>
+        {keywords.map((keyword, index) => (
+          <li
+            key={`${keyword}-${index}`}
+            className='w-max rounded-30 border border-white px-16 py-4 text-14-600'
+          >
+            {keyword}
+          </li>
+        ))}
+      </ul>
+      <footer>
         <Button onClick={handleClick}>대화 내용 전체보기</Button>
       </footer>
     </>
