0e23e3191d5816a0cd377ae891e6f2b65a997d60
hoon <dudrn587@gmail.com>
2025-03-10 22:44:04 +0900

    refactor: 대화 요약 페이지 로딩 페이지 연결

--- src/app/save-chat/[id]/Summary.tsx
+++ src/app/save-chat/[id]/Summary.tsx
@@ -5,12 +5,14 @@ import { useEffect, useState } from 'react'
 import WholeConversation from './WholeConversation'
 import { Conversation } from '@/utils/api/conversation/getConversation'
 import { postKeywords } from '@/utils/api/keywords/postKewords'
+import Loading from '@/app/loading'
 
 const Summary = ({ conversation }: { conversation: Conversation }) => {
   const { summary, conversationId } = conversation
   const [displayWholeConversation, setDisplayWholeConversation] =
     useState(false)
   const [keywords, setKeywords] = useState<string[]>([])
+  const [isKeywordsLoading, setIsKeywordsLoading] = useState(false)
 
   const handleClick = () => {
     setDisplayWholeConversation(true)
@@ -18,17 +20,25 @@ const Summary = ({ conversation }: { conversation: Conversation }) => {
 
   useEffect(() => {
     const getKeywords = async () => {
+      setIsKeywordsLoading(true)
       const data = await postKeywords(summary)
-      const foramttedKeywords = data.result.text
-        .replace(/["\\]/g, '')
-        .split(',')
-        .map((keyword: string) => keyword.trim())
+      const keywords = data.result.text
+      if (keywords) {
+        const foramttedKeywords = data.result.text
+          .replace(/["\\]/g, '')
+          .split(',')
+          .map((keyword: string) => keyword.trim())
+          .slice(0, 3)
 
-      setKeywords(foramttedKeywords)
+        setKeywords(foramttedKeywords)
+      }
+      setIsKeywordsLoading(false)
     }
 
     getKeywords()
   }, [])
+
+  if (isKeywordsLoading) return <Loading />
 
   return displayWholeConversation ? (
     <WholeConversation conversationId={conversationId} />
@@ -38,16 +48,17 @@ const Summary = ({ conversation }: { conversation: Conversation }) => {
         {summary}
       </main>
 
-      <ul className='flex gap-12 py-24'>
+      <ul className='flex flex-wrap items-center gap-12 py-24'>
         {keywords.map((keyword, index) => (
           <li
             key={`${keyword}-${index}`}
-            className='w-max rounded-30 border border-white px-16 py-4 text-14-600'
+            className='flex w-max items-center justify-center rounded-30 border border-white px-16 py-4 text-14-600'
           >
             {keyword}
           </li>
         ))}
       </ul>
+
       <footer>
         <Button onClick={handleClick}>대화 내용 전체보기</Button>
       </footer>
