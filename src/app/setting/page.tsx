a40584c7ec68601558b384987bb037278a513ae7
hoon <dudrn587@gmail.com>
2025-03-11 14:32:51 +0900

    설정 페이지 약관 및 정책 링크 추가

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -22,7 +22,7 @@ const Setting = async () => {
             src='assets/icons/button-prev-gray.svg'
             width={24}
             height={24}
-            alt='뒤로가기'
+            alt='뒤로 가기 버튼'
           />
         </Link>
         <Image
@@ -30,16 +30,23 @@ const Setting = async () => {
           src='assets/icons/setting-header.svg'
           width={73}
           height={38}
-          alt='설정 헤더'
+          alt='나의 정보'
         />
       </header>
 
-      <main className='flex-1'>
+      <section className='flex-1'>
         <div className='mt-60 w-full rounded-10 bg-white p-18 text-purple-40'>
           <p className='text-16-600-22'>{userInfo?.nickname || '사용자'}</p>
           <p className='text-14-500'>{formatDate(userInfo?.createdAt || '')}</p>
         </div>
-      </main>
+      </section>
+
+      <Link
+        href='https://chestnut-ellipse-bee.notion.site/1b172891bfeb8021b139ef873a24008f'
+        className='flex-1 text-14 text-[#D9D9D9]'
+      >
+        약관 및 정책
+      </Link>
 
       <LogoutButton />
     </div>
