4be07e3317626fe5b221bc70540a3eab6122dbc5
hoon <dudrn587@gmail.com>
2025-03-12 13:45:59 +0900

    refactor: 채팅 온보딩 페이지 tabIndex 적용

--- src/app/chat/onboarding/ChatOnboardingHeader.tsx
+++ src/app/chat/onboarding/ChatOnboardingHeader.tsx
@@ -21,20 +21,21 @@ const ChatOnboardingHeader = ({
             height={24}
             src='/assets/icons/button-prev-gray.svg'
             alt='뒤로가기'
+            tabIndex={1}
           />
           <Image
             className='ml-auto'
             width={289}
             height={32}
             src={progressBarSrc}
-            alt='상태바'
+            alt='상태 바'
           />
           <div className='h-16 w-16' />
         </header>
       ) : (
         <header className='flex justify-end p-24'>
-          <Link href='/' className='relative h-24 w-24 cursor-pointer'>
-            <Image fill src='/assets/icons/exit.svg' alt='나가기' />
+          <Link href='/' className='relative h-18 w-18 cursor-pointer'>
+            <Image fill src='/assets/icons/home.svg' alt='홈 버튼' />
           </Link>
         </header>
       )}
