55645cb71b3956a75935259b580e68e6aaa15958
hoon <dudrn587@gmail.com>
2025-03-25 21:14:19 +0900

    refactor: 리스트 가상화 tanstack virtual로 변경

--- src/app/chat/MessageList.tsx
+++ src/app/chat/MessageList.tsx
@@ -2,35 +2,82 @@ import MessageWrapper from './MessageWrapper'
 import UserMessage from './UserMessage'
 import AiMessage from './AiMessage'
 import { MessageType } from '@/utils/api/wholeConversation/getWholeConversation'
+import { ChatMode } from './Chat'
+import { useEffect, useMemo, useRef } from 'react'
+import { useVirtualizer } from '@tanstack/react-virtual'
 
 type MessageListProps = {
   messages: MessageType[]
   isLoading: boolean
 }
 
-const MessageList = ({ messages, isLoading }: MessageListProps) => {
-  return (
-    <div className='mt-16'>
-      {messages.map((message, index) => (
-        <MessageWrapper
-          key={index}
-          type={message.type}
-          content={message.message}
-        >
-          {message.type === 'USER' ? (
-            <UserMessage content={message.message} />
-          ) : (
-            <AiMessage content={message.message} />
-          )}
-        </MessageWrapper>
-      ))}
+const MessageList = ({
+  messages,
+  viewOnly,
+  chatMode,
+  isLoading,
+}: MessageListProps) => {
+  const containerRef = useRef<HTMLDivElement>(null)
 
-      {isLoading && (
-        <MessageWrapper type='AI'>
-          <AiMessage content='답변 중...' />
-        </MessageWrapper>
-      )}
-    </div>
+  const messagesWithTrigger = useMemo(() => {
+    const triggerMessage = {
+      type: 'AI' as MessageType['type'],
+      message: '',
+    }
+
+    return [triggerMessage, ...messages]
+  }, [messages])
+
+  const messagesCount = messagesWithTrigger.length
+
+  const rowVirtualizer = useVirtualizer({
+    count: messagesCount,
+    getScrollElement: () => containerRef.current,
+    estimateSize: (i) => 35,
+    overscan: 3,
+  })
+
+  useEffect(() => {
+    if (viewOnly) return
+
+    rowVirtualizer.scrollToIndex(messagesCount - 1)
+  }, [viewOnly, rowVirtualizer, chatMode, messagesCount])
+
+  const items = rowVirtualizer.getVirtualItems()
+
+  return (
+    <main ref={containerRef} className='flex-1 overflow-auto px-24'>
+      <div
+        className='relative'
+        style={{ height: `${rowVirtualizer.getTotalSize()}px` }}
+      >
+        {items.map((item) => {
+          const message = messagesWithTrigger[item.index]
+          return (
+            <div
+              key={item.key}
+              data-index={item.index}
+              ref={rowVirtualizer.measureElement}
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                transform: `translateY(${item.start}px)`,
+              }}
+            >
+              <MessageWrapper type={message.type} content={message.message}>
+                {message.type === 'USER' ? (
+                  <UserMessage content={message.message} />
+                ) : (
+                  <AiMessage content={message.message} />
+                )}
+              </MessageWrapper>
+            </div>
+          )
+        })}
+      </div>
+    </main>
   )
 }
 
