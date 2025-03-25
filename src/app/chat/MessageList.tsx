541e6478e73224eb4d3bbef2f1afa0278220ec8b
hoon <dudrn587@gmail.com>
2025-03-25 21:14:19 +0900

    refactor: 리스트 가상화 tanstack virtual로 변경

--- src/app/chat/MessageList.tsx
+++ src/app/chat/MessageList.tsx
@@ -2,14 +2,9 @@ import MessageWrapper from './MessageWrapper'
 import UserMessage from './UserMessage'
 import AiMessage from './AiMessage'
 import { MessageType } from '@/utils/api/wholeConversation/getWholeConversation'
-
-import {
-  VirtuosoMessageList,
-  VirtuosoMessageListLicense,
-} from '@virtuoso.dev/message-list'
 import { ChatMode } from './Chat'
-import useUpdateHeight from '@/hooks/useUpdateHeight'
-import useScrollToBottom from '@/hooks/useScrollToBottom'
+import { useEffect, useMemo, useRef } from 'react'
+import { useVirtualizer } from '@tanstack/react-virtual'
 
 type MessageListProps = {
   messages: MessageType[]
@@ -18,35 +13,71 @@ type MessageListProps = {
   chatMode?: ChatMode
 }
 
-const MessageList = ({ messages, viewOnly, chatMode }: MessageListProps) => {
-  const { containerHeight, containerRef } = useUpdateHeight({ chatMode })
-  const { virtuoso, messagesWithTrigger } = useScrollToBottom({
-    viewOnly,
-    messages,
+const MessageList = ({
+  messages,
+  viewOnly,
+  chatMode,
+  isLoading,
+}: MessageListProps) => {
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  const messagesWithTrigger = useMemo(() => {
+    const triggerMessage = {
+      type: 'AI' as MessageType['type'],
+      message: '',
+    }
+
+    return [triggerMessage, ...messages]
+  }, [messages])
+
+  const messagesCount = messagesWithTrigger.length
+
+  const rowVirtualizer = useVirtualizer({
+    count: messagesCount,
+    getScrollElement: () => containerRef.current,
+    estimateSize: (i) => 35,
+    overscan: 3,
   })
 
+  useEffect(() => {
+    if (viewOnly) return
+
+    rowVirtualizer.scrollToIndex(messagesCount - 1)
+  }, [viewOnly, rowVirtualizer, chatMode, messagesCount])
+
+  const items = rowVirtualizer.getVirtualItems()
+
   return (
-    <main className='flex flex-1 flex-col overflow-y-hidden p-24'>
-      <div ref={containerRef} className='relative flex-1'>
-        {containerHeight > 0 && (
-          <VirtuosoMessageListLicense licenseKey=''>
-            <VirtuosoMessageList
-              style={{ height: containerHeight }}
-              className='scrollbar-bar-hidden absolute inset-0 [&::-webkit-scrollbar]:hidden'
-              ref={virtuoso}
-              initialData={messagesWithTrigger}
-              ItemContent={({ data }) => (
-                <MessageWrapper type={data.type} content={data.message}>
-                  {data.type === 'USER' ? (
-                    <UserMessage content={data.message} />
-                  ) : (
-                    <AiMessage content={data.message} />
-                  )}
-                </MessageWrapper>
-              )}
-            />
-          </VirtuosoMessageListLicense>
-        )}
+    <main ref={containerRef} className='flex-1 overflow-auto px-24'>
+      <div
+        className='relative'
+        style={{ height: `${rowVirtualizer.getTotalSize()}px` }}
+      >
+        {items.map((item) => {
+          const message = messagesWithTrigger[item.index]
+          return (
+            <div
+              key={item.key}
+              data-index={item.index}
+              ref={rowVirtualizer.measureElement}
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                transform: `translateY(${item.start}px)`,
+              }}
+            >
+              <MessageWrapper type={message.type} content={message.message}>
+                {message.type === 'USER' ? (
+                  <UserMessage content={message.message} />
+                ) : (
+                  <AiMessage content={message.message} />
+                )}
+              </MessageWrapper>
+            </div>
+          )
+        })}
       </div>
     </main>
   )
