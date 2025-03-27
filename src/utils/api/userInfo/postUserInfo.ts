d9e9e0a3f26d13ad8c48e604e15bf8c1f234ecb4
tkddbs587 <162422277+tkddbs587@users.noreply.github.com>
2025-03-27 09:24:57 +0900

    Revert "Refactor: 리스트 가상화 tanstack virtual로 변경 후 적용"

--- src/utils/api/userInfo/postUserInfo.ts
+++ src/utils/api/userInfo/postUserInfo.ts
@@ -1,15 +1,11 @@
 import { UserInfo } from '@/app/chat/onboarding/page'
-import Cookies from 'js-cookie'
 
 export async function postUserInfo({ nickname, age, gender }: UserInfo) {
-  const accessToken = Cookies.get('accessToken1')
-
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}v1/members`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       Accept: 'application/json',
-      Authorization: `Bearer ${accessToken}`,
     },
 
     credentials: 'include',
